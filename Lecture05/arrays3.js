3acf3b8f85982b1e470266c0f8d045fff5c880e9
Arnav Gupta <arnav@codingblocks.com>
2017-09-08 02:23:43 +0530

    arrays part 3
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture05/arrays3.js
@@ -0,0 +1,20 @@
+// Arrays III - Data Changing
+// let notes = ['do', 're', 'mi', 'fa', 'so', 'la', 'ti', 'do']
+let notes = ['do', 're', 'mi']
+
+//slice
+// notes.slice(4,6)
+// console.log(notes)
+
+//splice
+// let omit = notes.splice(4, 2)
+// console.log(omit)
+// console.log(notes)
+
+// splice - part 2
+// notes.splice(0,3, 'sa', 're', 'ga')
+// console.log(notes)
+
+//concat
+console.log(notes.concat(['sa', 're', 'ga']))
+console.log(notes)
