4a8fe802b86120327db3fdaf358079394a0375b7
Arnav Gupta <arnav@codingblocks.com>
2017-09-07 20:44:30 +0530

    lec5
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture05/strings.js
@@ -0,0 +1,13 @@
+// String
+let p = 'arnav'
+let str = `this is ${p}'s string`
+let longStr = 'this is \n \
+a very long\
+string'
+
+// Searching
+
+let s = 'this is a easy job'
+let s2 = 'ab aab abbcd ba aab'
+console.log(s2.lastIndexOf('ab', 16))
+
---
+++ Lecture05/strings2.js
@@ -0,0 +1,6 @@
+// String Methods
+let mainStr = "this-is-a-long-string"
+
+let smallStr = mainStr.substr(-6,4)
+
+console.log(smallStr)
