d42754f7a7b8f2e2dc05d30e56b57bf69827aa26
Arnav Gupta <arnav@codingblocks.com>
2017-09-07 22:25:00 +0530

    arrays 2
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture05/arrays2.js
@@ -0,0 +1,15 @@
+// Arrays - Part 2 - Add/Remove items
+let fruits = ["apple", "mango"]
+
+//push and pop
+fruits.push("melon", "kiwi")
+console.log(fruits)
+// fruits.pop()
+// console.log(fruits)
+
+//shift and unshift
+// fruits.shift();
+// console.log(fruits)
+//
+// fruits.unshift("kiwi", "grapes")
+// console.log(fruits)
