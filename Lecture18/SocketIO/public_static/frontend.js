3a9a508eff6623658f5f08cc007b74463fe95603
Arnav Gupta <arnav@codingblocks.com>
2017-10-28 15:07:03 +0530

    socket io
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture18/SocketIO/public_static/frontend.js
+++ Lecture18/SocketIO/public_static/frontend.js
@@ -1,6 +1,11 @@
 var socket = io();
+function play(instId) {
+    socket.emit('play', {id: instId})
+}
 
 $(function () {
+    var usernameBox = $('#username')
+    var loginBtn = $('#login')
     var sendBtn = $('#send');
     var msgBox = $('#message');
     var chats = $('#chats');
@@ -10,10 +15,22 @@ $(function () {
             message: msgBox.val()
         })
     })
+    loginBtn.click(function () {
+        socket.emit('login', {username: usernameBox.val()})
+    })
+    socket.on('logged_in', function () {
+        $('#loginbox').css('display', 'none')
+        $('#musicbox').css('display', 'block')
+        $('#chatbox').css('display', 'block')
+    })
     socket.on('msg', function(data) {
         chats.append(
             `<li>${data.sender} : ${data.message}</li>`
         )
+    })
+    socket.on('play', function (data) {
+        var instrument = $('#a'+data.id)[0]
+        instrument.play();
     })
 
 })
--- Lecture18/SocketIO/server.js
+++ Lecture18/SocketIO/server.js
@@ -9,18 +9,29 @@ const server = http.Server(app)
 //The socket.io server
 const io = socketio(server)
 
+const users = {}
+
+const PORT = process.env.PORT || 2323;
+
 io.on('connection', function (socket) {
     console.log("Socket created :" + socket.id)
     socket.on('msg', function (data) {
-        io.emit('msg', {
-            sender: socket.id,
+        socket.broadcast.emit('msg', {
+            sender: users[socket.id],
             message: data.message
         })
     })
+    socket.on('login', function (data) {
+        users[socket.id] = data.username
+        socket.emit('logged_in')
+    })
+    // socket.on('play', function(data) {
+    //     io.emit('play', data)
+    // })
 })
 
 app.use('/', express.static(__dirname + "/public_static"))
 
-server.listen(2323, () => {
-    console.log("Server started at http://localhost:2323")
+server.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`)
 })
